d5696e6a2dba8c2ed37b1bcb6446122f9d6dd4bf
javierli0829 <a2982601@gmail.com>
2024-10-08 21:24:03 +0800

    successfully connect to the server

--- src/app/transaction-input/transaction-input.page.ts
+++ src/app/transaction-input/transaction-input.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { addIcons } from 'ionicons';
-import { closeCircleOutline } from 'ionicons/icons';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-transaction-input',
@@ -10,10 +9,27 @@ import { closeCircleOutline } from 'ionicons/icons';
 })
 export class TransactionInputPage {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
+
+  private apiUrl = 'http://localhost:3000/api'; // 你的 Express API URL
 
   goBack() {
     this.router.navigate(['/home']);
   }
 
+  getUsers() {
+    this.http.get(this.apiUrl + "/users").subscribe(data => {
+      console.log(data);
+    })
+    this.addTransaction();
+  }
+
+  addTransaction () {
+    this.http.post(this.apiUrl + "/users/transaction", {type: "Test", amount: 100, description: "Test", timestamp: new Date().toDateString()}).subscribe(res =>{
+      console.log('Transaction created:', res);
+    },
+    error => {
+      console.error('Error creating transaction:', error);
+    })
+  }
 }
